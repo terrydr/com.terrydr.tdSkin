84bde58fb27074ead07ae674f81e1a363e680cfe
路亮亮 <luliangliang@luliangliangdeMac-mini.local>
2016-02-19 13:55:45 +0800

    js

--- www/JRcamera.js
+++ www/JRcamera.js
@@ -1,14 +1,10 @@
-var cordova=require('cordova');
-
-var  JRCamera =function(){
+var jrCamera = {
     
-    JRCamera.prototype.jrTakePhotos=function(success,error,str){
-        
-        cordova.exec(sucess,error,'JRCamera','jrTakePhotos',str)//'Echo'对应我们在java文件中定义的类名，echo对应我们在这个类中调用的自定义方法，str是我们客户端传递给这个方法的参数，是个数组
-        
-    }
+jrTakePhotos: function(urlScheme, successCallback, errorCallback) {
     
+    cordova.exec(successCallback,errorCallback,'JRCamera','jrTakePhotos',[urlScheme]);
+});
 }
-var  jrCamera=new JRCamera();
 
-module.exports=jrCamera;
+
+module.exports = jrCamera;
