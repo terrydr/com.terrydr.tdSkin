30428582123c029421adb713cd66a7a28785e199
路亮亮 <luliangliang@luliangliangdeMac-mini.local>
2016-02-25 10:42:19 +0800

    JS

--- www/JRcamera.js
+++ www/JRcamera.js
@@ -1,16 +1,40 @@
 var jrCamera = {
-    
 jrTakePhotos: function(successCallback, errorCallback) {
     console.log("invoked");
-	cordova.exec(
-            successCallback,
-            errorCallback,
-            "JRCamera",
-            "jrTakePhotos",
-	        []
-        );
-
+    cordova.exec(
+                 successCallback,
+                 errorCallback,
+                 "JRCamera",
+                 "jrTakePhotos",
+                 []
+                 );
+    
 }
+    
+jrCleanWithPath: function(successCallback, errorCallback,filePath) {
+    console.log("jrCleanWithPath");
+    cordova.exec(
+                 successCallback,
+                 errorCallback,
+                 "JRCamera",
+                 "jrCleanDataWithPath",
+                 [filePath]
+                 );
+    
+}
+    
+jrCleanAllData: function(successCallback, errorCallback) {
+    console.log("jrCleanAllData");
+    cordova.exec(
+                 successCallback,
+                 errorCallback,
+                 "JRCamera",
+                 "jrCleanAllData",
+                 []
+                 );
+    
+}
+    
 }
 
 
